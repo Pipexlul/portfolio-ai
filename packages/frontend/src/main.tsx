df1043a58fd59a7932e61511085aa2fc70f7e4c7
Pipexlul <felipgg@gmail.com>
2023-07-13 19:45:37 -0400

    fix(frontend): fix loading order tailwind and mantine components to work in harmony

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { MantineProvider } from "@mantine/core";
+
+import App from "./App.tsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider>
+      <App />
+    </MantineProvider>
   </React.StrictMode>,
-)
+);
