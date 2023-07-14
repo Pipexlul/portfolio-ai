b0ab02982dce0030e51ee93ba8563868463cecc5
Pipexlul <felipgg@gmail.com>
2023-07-13 20:51:12 -0400

    chore(frontend): fix pre-commit hook config files inside frontend package

--- packages/frontend/.eslintrc.cjs
+++ packages/frontend/.eslintrc.cjs
@@ -27,10 +27,18 @@ module.exports = {
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
-    project: "./tsconfig.json",
+    project: "tsconfig.json",
+    tsconfigRootDir: __dirname,
   },
   plugins: ["@typescript-eslint", "react"],
   rules: {
     "react/react-in-jsx-scope": 0,
+    "react/function-component-definition": [
+      2,
+      {
+        namedComponents: "arrow-function",
+        unnamedComponents: "arrow-function",
+      },
+    ],
   },
 };
